126f9c08846605d7324aa6c53a39eafe504264bc
David Armendariz <darmendariz1998@outlook.com>
2020-12-25 16:46:09 -0500

    Fixed jest file

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,15 @@
 export default {
-  collectCoverageFrom: [
-    'src/**/*.ts'
-  ],
+  collectCoverageFrom: ['src/**/*.ts'],
   coverageThreshold: {
     global: {
       branches: 100,
       functions: 100,
       lines: 100,
-      statements: 100
-    }
+      statements: 100,
+    },
   },
   moduleDirectories: ['node_modules', 'src'],
-  roots: [
-    '<rootDir>/src'
-  ],
+  roots: ['<rootDir>/src'],
   preset: 'ts-jest',
-  testEnvironment: 'node'
-}
+  testEnvironment: 'node',
+};
